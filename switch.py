76c7e511451993502a83a447d52bfe297f64c63f
Adrian <neacsu_adrian@yahoo.com>
2024-10-28 13:06:15 +0200

    bdpu implemented

--- switch.py
+++ switch.py
@@ -30,10 +30,89 @@ def create_vlan_tag(vlan_id):
     # vlan_id & 0x0FFF ensures that only the last 12 bits are used
     return struct.pack('!H', 0x8200) + struct.pack('!H', vlan_id & 0x0FFF)
 
-def send_bdpu_every_sec():
+def send_bdpu_every_sec(root_bridge_ID, own_bridge_ID, interface_object, interfaces):
     while True:
         # TODO Send BDPU every second if necessary
+        if root_bridge_ID == own_bridge_ID:
+            length = 416     
+            dest_mac = bytes.fromhex("0180C2000000")             # 6b
+            src_mac = get_switch_mac()                           # 6b
+            llc_length = struct.pack('!H', length)               # 2b
+            llc_header = bytes.fromhex("424203")                 # 3b
+            protocol_id = bytes.fromhex("0000")                  # 2b
+            protocol_version_id = bytes.fromhex("00")            # 1b
+            bpdu_type = bytes.fromhex("00")                      # 1b
+            flags = bytes.fromhex("00")                          # 1b
+            root_id = struct.pack('!Q', int(root_bridge_ID))     # 8b
+            root_path_cost = bytes.fromhex("00000000")           # 4b
+            bridge_id = struct.pack('!Q', int(root_bridge_ID))   # 8b
+            # port_ID                                            # 2b
+            message_age = struct.pack('!H', 2)                   # 2b
+            max_age = struct.pack('!H', 20)                      # 2b
+            hello_time = struct.pack('!H', 2)                    # 2b
+            forward_delay = struct.pack('!H', 15)                # 2b
+            for i in interfaces:
+                if interface_object[i].vlan == -2:
+                    port_id = struct.pack('!H', i)
+                    data = (
+                        dest_mac + src_mac + llc_length + llc_header +
+                        protocol_id + protocol_version_id + bpdu_type + flags +
+                        root_id + root_path_cost + bridge_id + port_id +
+                        message_age + max_age + hello_time + forward_delay
+                    )
+                    send_to_link(i, length, data)
+
+        # Send BPDU on all trunk ports with:
+        #     root_bridge_ID = own_bridge_ID
+        #     sender_bridge_ID = own_bridge_ID
+        #     sender_path_cost = 0
+        
         time.sleep(1)
+
+def receive_bdpu_frame(length, data, root_bridge_ID, own_bridge_ID, cost, interface_object, interfaces, interface):
+    bdpu_root_id = int.from_bytes(data[22:30], byteorder='big')
+    bdpu_root_cost = int.from_bytes(data[30:34], byteorder='big')
+    bdpu_bridge_id = int.from_bytes(data[34:42], byteorder='big')
+    root_port = int.from_bytes(data[42:44], byteorder='big')
+    is_root = False
+
+    if root_bridge_ID == own_bridge_ID:
+        is_root = True
+
+    if bdpu_root_id < root_bridge_ID:
+        root_bridge_ID = bdpu_root_id
+        cost = bdpu_root_cost + 10
+    
+        if is_root == True:
+            for i in interfaces:
+                if interface_object[i].vlan == -2:
+                    interface_object[i].status = 0
+
+        if interface_object[root_port].status == 0:
+            interface_object[root_port].status = 1
+        
+        for i in interfaces:
+            print(interface_object[i].vlan)
+            if interface_object[i].vlan == -2 and i != root_port:
+                data_mutable = bytearray(data)
+                data_mutable[34:42] = struct.pack('!Q', own_bridge_ID)
+                data_mutable[42:44] = struct.pack('!H', cost)
+                send_to_link(i, length, bytes(data_mutable))
+    elif bdpu_root_id == root_bridge_ID:
+        if interface == root_port and bdpu_root_cost + 10 < cost:
+            cost = bdpu_root_cost + 10
+        elif interface != root_port:
+            if bdpu_root_cost > cost:
+                interface_object[interface].status = 1
+    elif bdpu_bridge_id == own_bridge_ID:
+        interface_object[interface].status = 0
+
+    if own_bridge_ID == root_bridge_ID:
+        for i in interfaces:
+            interface_object[i].status = 1
+
+    return root_bridge_ID, own_bridge_ID, cost
+
 
 def parse_configure(switch_id):
     print(switch_id)
@@ -63,34 +142,68 @@ def get_vlan_from_source(interface, vlan_id_table, vlan, data, length):
         data, length = remove_vlan_tag(data, length)
         return vlan, data, length
 
-def send_frame(interface, length, data, vlan_id_table, vlan):
-    vlan_dest = vlan_id_table[get_interface_name(interface)]
-    print(vlan_dest, " ", vlan)
+def send_frame(interface, length, data, interface_object, vlan):
+    if interface_object[interface].status == 0:
+        return
+
+    vlan_dest = interface_object[interface].vlan
     if vlan_dest == -2:
         data, length = add_vlan_tag(vlan, data, length)
         send_to_link(interface, length, data)
     elif vlan_dest == vlan:
-        print(get_interface_name(interface))
         send_to_link(interface, length, data)
 
+def receive_frame(dest_mac, src_mac, mac_table, length, data, interface_object, vlan_id, interfaces, interface):
+    if dest_mac != "ff:ff:ff:ff:ff:ff":
+        mac_table[src_mac] = interface
+        if dest_mac in mac_table:
+            # send_to_link(mac_table[dest_mac], length, data)
+            send_frame(mac_table[dest_mac], length, data, interface_object, vlan_id)
+        else:
+            for o in interfaces:
+                if o != interface:
+                    # send_to_link(o, length, data)
+                    send_frame(o, length, data, interface_object, vlan_id)
+    else:
+        for o in interfaces:
+                if o != interface:
+                    # send_to_link(o, length, data)
+                    send_frame(o, length, data, interface_object, vlan_id)
+
+class Interface:
+  def __init__(self, name, status, vlan):
+    self.name = name
+    self.status = status
+    self.vlan = vlan
 
 def main():
     # init returns the max interface number. Our interfaces
     # are 0, 1, 2, ..., init_ret value + 1
     switch_id = sys.argv[1]
     priority, vlan_id_table = parse_configure(switch_id)
+    own_bridge_ID =  int(priority)
+    root_bridge_ID = own_bridge_ID
+    root_path_cost = 0
+    
     print(priority)
     print(vlan_id_table)
 
     num_interfaces = wrapper.init(sys.argv[2:])
     print(num_interfaces)
     interfaces = range(0, num_interfaces)
+    interface_object = {}
+    for i in interfaces:
+        if vlan_id_table[get_interface_name(i)] == -2:
+            interface_object[i] = Interface(get_interface_name(i), 0, vlan_id_table[get_interface_name(i)])
+        else:
+            interface_object[i] = Interface(get_interface_name(i), 1, vlan_id_table[get_interface_name(i)])
+
 
     print("# Starting switch with id {}".format(switch_id), flush=True)
     print("[INFO] Switch MAC", ':'.join(f'{b:02x}' for b in get_switch_mac()))
 
     # Create and start a new thread that deals with sending BDPU
-    t = threading.Thread(target=send_bdpu_every_sec)
+    t = threading.Thread(target=send_bdpu_every_sec, args=(root_bridge_ID, own_bridge_ID, interface_object, interfaces))
     t.start()
 
     # Printing interface names
@@ -103,6 +216,10 @@ def main():
         # b1 = bytes([72, 101, 108, 108, 111])  # "Hello"
         # b2 = bytes([32, 87, 111, 114, 108, 100])  # " World"
         # b3 = b1[0:2] + b[3:4].
+        if own_bridge_ID == root_bridge_ID:
+            for i in interfaces:
+                interface_object[i].status = 1
+        
         interface, data, length = recv_from_any_link()
 
         dest_mac, src_mac, ethertype, vlan_id = parse_ethernet_header(data)
@@ -126,21 +243,11 @@ def main():
         # TODO: Implement forwarding with learning
         # TODO: Implement VLAN support
         # TODO: Implement STP support
-        if dest_mac != "ff:ff:ff:ff:ff:ff":
-            mac_table[src_mac] = interface
-            if dest_mac in mac_table:
-                # send_to_link(mac_table[dest_mac], length, data)
-                send_frame(mac_table[dest_mac], length, data, vlan_id_table, vlan_id)
-            else:
-                for o in interfaces:
-                    if o != interface:
-                        # send_to_link(o, length, data)
-                        send_frame(o, length, data, vlan_id_table, vlan_id)
+        if dest_mac == "01:80:c2:00:00:00":
+            root_bridge_ID, own_bridge_ID, root_path_cost = receive_bdpu_frame(length,
+             data, root_bridge_ID, own_bridge_ID, root_path_cost, interface_object, interfaces, interface)
         else:
-            for o in interfaces:
-                    if o != interface:
-                        # send_to_link(o, length, data)
-                        send_frame(o, length, data, vlan_id_table, vlan_id)
+            receive_frame(dest_mac, src_mac, mac_table, length, data, interface_object, vlan_id, interfaces, interface)
 
         # data is of type bytes.
         # send_to_link(i, length, data)
